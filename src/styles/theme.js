967c386d001eeed56eeed1981cd4325e74b584e6
Daniel Sobrinho <dev.sobrinho@gmail.com>
2021-11-19 12:09:34 -0300

    Styles global, theme and render-theme for tests

---
+++ src/styles/theme.js
@@ -0,0 +1,54 @@
+export const theme = {
+  colors: {
+    primaryCOlorDark: 'rgb(8 22 66)',
+    primaryColor: '#0A1128',
+    secondaryColor: '#DC143C',
+    white: '#FFFFFF',
+    dark: '#000000',
+    logoColor: '#35495e',
+    mediumGray: '#DDDDDD',
+  },
+  sizes: {
+    small: '2.8rem',
+    medium: '6.4rem',
+    large: '12.8rem',
+  },
+  fonts: {
+    family: {
+      default: "'Open Sans', sans-serif",
+      primary: "'Comfortaa', cursive",
+      secondary: "'Roboto', sans-serif",
+    },
+    weight: {
+      // lighter: 200,
+      light: 300,
+      normal: 400,
+      bold: 700,
+      bolder: 900,
+    },
+    sizes: {
+      xsmall: '1.1rem',
+      small: '1.3rem',
+      medium: '1.5rem',
+      large: '1.7rem',
+      // xlarge: '2.2rem',
+      xxlarge: '3.6rem',
+      // huge: '4rem',
+      xhuge: '4.6rem',
+    },
+  },
+  media: {
+    lteMedium: '(min-width: 768px)',
+    lteSmall: '(min-width: 414px)',
+  },
+  spacings: {
+    xsmall: '0.6rem',
+    small: '0.8rem',
+    medium: '1.2rem',
+    large: '1.6rem',
+    xlarge: '2.4rem',
+    xxlarge: '3.2rem',
+    huge: '4.8rem',
+    // xhuge: '5.6rem',
+  },
+};
