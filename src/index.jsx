10dfa59b6bce344a7ea713b88fb41c5ebcf10409
Daniel Sobrinho <dev.sobrinho@gmail.com>
2021-11-19 23:34:35 -0300

    APP: Implemented Component <ToastContainer /> of lib react-toastify

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
+import { ToastContainer, toast } from 'react-toastify';
 
 import { GlobalStyles } from './styles/global-styles';
 import { theme } from './styles/theme';
 
+import 'react-toastify/dist/ReactToastify.css';
+
 import Home from './templates/Home';
+import { Register } from './templates/Register';
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Home />
+      {/* <Home /> */}
+      <Register />
       <GlobalStyles />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
