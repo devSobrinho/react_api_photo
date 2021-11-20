3a38bd3cd7f75d146f31035778e3173a06f520d0
Daniel Sobrinho <dev.sobrinho@gmail.com>
2021-11-20 16:55:05 -0300

    Implematation component <BrowserRouter/> and <Routes /> of lib react-router-dom for functionality of the routes on the page

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 
 import { GlobalStyles } from './styles/global-styles';
@@ -10,24 +11,26 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import Home from './templates/Home';
 import { Register } from './templates/Register';
+import { Routes } from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      {/* <Home /> */}
-      <Register />
-      <GlobalStyles />
-      <ToastContainer
-        position="top-right"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
+      <BrowserRouter>
+        <Routes />
+        <GlobalStyles />
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
